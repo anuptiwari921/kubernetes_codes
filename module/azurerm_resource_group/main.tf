9a5c16aa17db6a2086a27be17502f2ce16aa62fa
Anup Tiwari (Nokia) <anup.tiwari@nokia.com>
2025-07-07 15:51:34 +0530

    added one more rg

--- module/azurerm_resource_group/main.tf
+++ module/azurerm_resource_group/main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "todo-rg1988" {
     name = var.resource_group_name
     location = var.location_name
 }
+resource "azurerm_resource_group" "todo-rg2000" {
+    name = var.resource_group_name
+    location = var.location_name
+}
